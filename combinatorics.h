d0d66792cc4856f43f792fac03ad1a3737aea046
Nutsheil <62464817+Nutsheil@users.noreply.github.com>
2021-03-26 16:46:19 +0300

    Add files via upload

---
+++ combinatorics.h
@@ -0,0 +1,18 @@
+#ifndef COMBINATORICS_H
+#define COMBINATORICS_H
+
+typedef unsigned int UI;
+
+UI U(UI m, UI n);
+
+UI P(UI n);
+
+UI A(UI m, UI n);
+
+UI C(UI m, UI n);
+
+UI S(UI m, UI n);
+
+UI B(UI n);
+
+#endif // COMBINATORICS_H
